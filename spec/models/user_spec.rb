746ec04e9d29b20bb58b4d9dc0e7a6b558f4526b
SaboorSohaib <abdulsaboorsohaib@gmail.com>
2023-04-24 22:46:15 +0430

    ADD TEST FOR USER MODEL

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject { User.create!(name: 'John Doe', email: 'john@doe.com', password: '123433') }
+
+  before { subject.save }
+
+  it "Should test the name attribute" do
+    expect(subject.name).to eq('John Doe')
+  end
+
+  it "Should test the email attribute" do
+    subject.email = nil
+    expect(subject).to_not be_valid
+  end
+
+  it "Should test the password attribute" do
+    expect(subject.password).to_not be_nil
+  end
 end
