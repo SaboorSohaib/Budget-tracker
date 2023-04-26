c5577db812035cfdde3a71c8dd6441f3a9eda63e
SaboorSohaib <abdulsaboorsohaib@gmail.com>
2023-04-26 22:13:05 +0430

    ADD TESTS FOR GROUPS EXPENCES AND USERS

---
+++ spec/feature/expenses/index.html.erb_spec.rb
@@ -0,0 +1,48 @@
+require 'rails_helper'
+include Warden::Test::Helpers
+
+RSpec.describe 'expenses/index.html.erb', type: :feature do
+  before(:each) do
+    @user = User.create!(name: 'User', email: 'user1@gmail.com', password: '432323')
+    @group = Group.create!(name: 'Group', icon: 'icon', user_id: @user.id)
+    @expense = Expense.create!(name: 'Expense', amount: 100, user_id: @user.id)
+    @expense = Expense.create!(name: 'Expense1', amount: 200, user_id: @user.id)
+  end
+
+  it 'should display page name' do
+    login_as(@user, :scope => :user)
+    visit group_expenses_path(@group.id)
+    expect(page).to have_content('Transactions')
+  end
+
+  it 'should display the icon' do
+    login_as(@user, :scope => :user)
+    visit group_expenses_path(@group.id)
+    expect(page).to have_content('Icon')
+  end
+
+  it 'displays the total amount of expenses' do
+    login_as(@user, :scope => :user)
+    visit group_expenses_path(@group)
+    total_amount = @group.expenses.sum(:amount)
+    expect(page).to have_content("Total Amount: #{total_amount} $")
+  end
+
+  it 'displays the list of expenses' do
+    login_as(@user, :scope => :user)
+    visit group_expenses_path(@group)
+
+    @expenses = @group.expenses.order(created_at: :desc)
+    @expenses.each do |expense|
+      expect(page).to have_content("Transaction Name: #{expense.name}")
+      expect(page).to have_content("Transaction Amount: #{expense.amount} $")
+    end
+  end
+
+  it 'has a link to add a new transaction' do
+    login_as(@user, :scope => :user)
+    visit group_expenses_path(@group)
+
+    expect(page).to have_link('Add a New Transaction', href: new_group_expense_path(@group))
+  end
+end
