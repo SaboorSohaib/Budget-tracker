511290182ae1dcb5d7fd704e36e4aff8c1774f84
SaboorSohaib <abdulsaboorsohaib@gmail.com>
2023-04-27 10:34:30 +0430

    FIX LINTER ERRORS

--- spec/feature/expenses/index.html.erb_spec.rb
+++ spec/feature/expenses/index.html.erb_spec.rb
@@ -1,7 +1,10 @@
 require 'rails_helper'
-include Warden::Test::Helpers
+module WardenTestHelpers
+  include Warden::Test::Helpers
+end
 
 RSpec.describe 'expenses/index.html.erb', type: :feature do
+  include WardenTestHelpers
   before(:each) do
     @user = User.create!(name: 'User', email: 'user1@gmail.com', password: '432323')
     @group = Group.create!(name: 'Group', icon: 'icon', user_id: @user.id)
@@ -10,26 +13,26 @@ RSpec.describe 'expenses/index.html.erb', type: :feature do
   end
 
   it 'should display page name' do
-    login_as(@user, :scope => :user)
+    login_as(@user, scope: :user)
     visit group_expenses_path(@group.id)
     expect(page).to have_content('Transactions')
   end
 
   it 'should display the icon' do
-    login_as(@user, :scope => :user)
+    login_as(@user, scope: :user)
     visit group_expenses_path(@group.id)
     expect(page).to have_content('Icon')
   end
 
   it 'displays the total amount of expenses' do
-    login_as(@user, :scope => :user)
+    login_as(@user, scope: :user)
     visit group_expenses_path(@group)
     total_amount = @group.expenses.sum(:amount)
     expect(page).to have_content("Total Amount: #{total_amount} $")
   end
 
   it 'displays the list of expenses' do
-    login_as(@user, :scope => :user)
+    login_as(@user, scope: :user)
     visit group_expenses_path(@group)
 
     @expenses = @group.expenses.order(created_at: :desc)
@@ -40,7 +43,7 @@ RSpec.describe 'expenses/index.html.erb', type: :feature do
   end
 
   it 'has a link to add a new transaction' do
-    login_as(@user, :scope => :user)
+    login_as(@user, scope: :user)
     visit group_expenses_path(@group)
 
     expect(page).to have_link('Add a New Transaction', href: new_group_expense_path(@group))
--- spec/feature/groups/index.html.erb_spec.rb
+++ spec/feature/groups/index.html.erb_spec.rb
@@ -11,12 +11,12 @@ RSpec.describe 'groups/index.html.erb', type: :feature do
     fill_in 'Email', with: 'user@gmail.com'
     fill_in 'Password', with: '2132424'
     click_button 'Log in'
-    
+
     visit groups_path
     expect(page).to have_content(@user.name)
     expect(page).to have_content(@group.name)
     expect(page).to have_content(@group.expenses.sum(:amount))
-    expect(page).to have_content(@group.created_at.strftime("%d %b %Y"))
+    expect(page).to have_content(@group.created_at.strftime('%d %b %Y'))
     expect(page).to have_content('Add a New Category')
   end
 end
